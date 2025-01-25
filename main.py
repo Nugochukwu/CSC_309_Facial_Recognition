663ec63a3eb66eb4bbba18137f1457df9c4d8558
N.Ugochukwu <115807244+Nugochukwu@users.noreply.github.com>
2025-01-25 11:58:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from src.face_matching import match_faces
 
 def main():
     # Define the image path
-    image_path = 'pictures/PFP.jpg'
+    image_path = 'pictures/man1.jpg'
+    imageOutput_dir = 'dataset/'
     try:
-        detect_face_and_display(image_path)
+        detect_face_and_display(image_path,imageOutput_dir)
         extract_features()
         match_faces()
     except FileNotFoundError as e:
--- src/face_detection.py
+++ src/face_detection.py
@@ -1,8 +1,13 @@
 import cv2
 import matplotlib.pyplot as plt
+import os
 
 
-def detect_face_and_display(image_path):
+
+def detect_face_and_display(image_path, output_dir):
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     # Read the image
     img = cv2.imread(image_path)
     # Check if the image was successfully loaded
@@ -24,6 +29,19 @@ def detect_face_and_display(image_path):
     face = face_classifier.detectMultiScale(
         gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
     )
+    # Check if faces are detected
+    if len(face) == 0:
+        print("No faces detected.")
+        return
+
+    print(f"Number of faces detected: {len(face)}")
+    # Loop through detected faces and save each as a separate image
+    for i, (x, y, w, h) in enumerate(face):
+        face_img = img[y:y + h, x:x + w]  # Crop the face from the original image
+        face_file_path = os.path.join(output_dir, f"face_{i + 1}.png")
+        cv2.imwrite(face_file_path, face_img)  # Save the cropped face
+        print(f"Exported face {i + 1} to {face_file_path}")
+
     # Draw a Bounding Box
     for (x, y, w, h) in face:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
